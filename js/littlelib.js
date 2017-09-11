eb24c433c98b8921b2e4952b05859098873b8dc2
Guilherme Cruz <sambrmg@gmail.com>
2017-09-11 16:53:56 -0300

    3th level menu

--- js/littlelib.js
+++ js/littlelib.js
@@ -1,28 +1,39 @@
-var jsonMenu = {
-            1: { "label": "item 1", "href": "" },
-            2: { "label": "item 2", "href": "" },
-            3: { "label": "item 3", "href": "" },
-            4: { "label": "item 4", "href": "" },
-            5: { "label": "item 5", "href": "" },
-            6: { "label": "item 6", "href": "" },
-            7: { "label": "item 7", "href": "" },
-            8: { "label": "item 8", "href": "" },
-            9: { "label": "item 9", "href": "" }
-    };
+var jsonMenu = [
+    { 
+        "label": "item 1", 
+        "href": "#",
+        "n2": {
+                "label":"subitem 1", 
+                "href": "#",
+                "n3": {
+                    "label":"sub-subitem 1",
+                    "href": "#"
+                }
+            } 
+    },
+    { "label": "item 2", "href": "#" },
+    { "label": "item 3", "href": "#" },
+    { "label": "item 4", "href": "#" },
+    { "label": "item 5", "href": "#" },
+    { "label": "item 6", "href": "#" },
+    { "label": "item 7", "href": "#" },
+    { "label": "item 8", "href": "#" },
+    { "label": "item 9", "href": "#" }
+];
 
-var llMainMenu = document.querySelector(".ll-main-menu ul");
-
-function llAddLiToUl(jsonMenu, llMainMenu){
-    for(var i in jsonMenu) { 
-        if (jsonMenu.hasOwnProperty(i)) {
+function llAddLiToUl(obj){
+    var llMainMenu = document.querySelector(".ll-main-menu ul");
+    for(var i in obj) { 
+        if (obj.hasOwnProperty(i)) {
             var li = document.createElement("li");  
             var a = document.createElement("a");  
-            var text  = document.createTextNode(jsonMenu[i].label);
+            var text  = document.createTextNode(obj[i].label);
             a.appendChild(text);
             li.appendChild(a);
-
+            console.log(a);
+            console.log(li);
             llMainMenu.appendChild(li);
         }
      }
 }
-llAddLiToUl(jsonMenu, llMainMenu);
+//llAddLiToUl(jsonMenu);
