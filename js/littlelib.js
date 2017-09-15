e8c46de02b7f49ffca5b079618c45546f2bf0f76
Guilherme Cruz <sambrmg@gmail.com>
2017-09-15 17:28:59 -0300

    keypress 13 preventDefault

--- js/littlelib.js
+++ js/littlelib.js
@@ -42,9 +42,7 @@ function findInNodes(jsonObj, label){
         var res = re.exec(jsonObj[i].label);
         if(res !== null){
             newObj.push(jsonObj[i]);
-            
         }
-        
     }
     return newObj;
 }
@@ -52,16 +50,11 @@ function findInNodes(jsonObj, label){
 function createMenu(jsonObj){
     var llMainMenu = document.querySelector(".ll-main-menu");
     llMainMenu.appendChild(createNodeUl(jsonObj, ""));
-
     addEventClickHref();
-    
 }
-
-
 
 function createNodeUl(jsonObj, ul){
     var elUl = document.createElement("ul");
-    
     for(var i in jsonObj) {
         if (jsonObj.hasOwnProperty(i)) {
             if(typeof jsonObj[i].n === "undefined"){
@@ -71,16 +64,13 @@ function createNodeUl(jsonObj, ul){
                 elUl.appendChild(createNodeLi(jsonObj[i].label, jsonObj[i].href, ul));
             }
         }
-        
     }
-
     return elUl;
 }
 
 function createNodeLi(label, href, ul){
     var elA = createElementAhref(label, href);
     var elLi = createElementLi(elA, ul);
-
     return elLi;
 }
 
@@ -89,7 +79,6 @@ function createElementAhref(label, href){
     var text = document.createTextNode(label);
     elA.appendChild(text);    
     elA.setAttribute('href', href);
-
     return elA;
 }
 
@@ -99,7 +88,6 @@ function createElementLi(elA, ul){
     if(ul !== ""){
         elLi.appendChild(ul);
     }
-    
     return elLi;
 }
 
@@ -111,50 +99,41 @@ function addEventClickHref(){
         }
         allUlMainMenu[i].addEventListener('click', 
             function (event) {
-                
             var el = event.target;
             var elHref = el.getAttribute('href').trim();
-
             if(elHref === "#" || 
                 elHref == ""){
-
                 event.preventDefault();
             }
-            
             if( el.classList.contains("icon-plus") || 
                 el.classList.contains("icon-minus") ){
-
                 el.classList.toggle("icon-plus");
                 el.classList.toggle("icon-minus");
             }
-            
             el.nextSibling.classList.toggle("open"); 
         });
     }
 }
 
 function initMenu(jsonObjMenu){
-   
-
     createMenu(jsonObjMenu);
     jsonObjMenu = jsonObjMenu;
-    
-    
-
-    document.querySelector("#searchMenu").addEventListener('keyup', 
+    document.querySelector("#searchMenu").addEventListener('keypress', 
     function (event) {
         if(event.keyCode == 13){
             event.preventDefault();
         }
+    });
+    document.querySelector("#searchMenu").addEventListener('keyup', 
+    function (event) {
+        
         if(event.target.value.trim().length > 0){
             var newObj = findInNodes(jsonObjMenu, event.target.value);
             clearAllUlMenu();
             createMenu(newObj);
-            
         }else{
             clearAllUlMenu();
             createMenu(jsonObjMenu);
-            
         }
         
     });
