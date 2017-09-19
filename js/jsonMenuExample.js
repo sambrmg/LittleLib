428e3eca2cb94d64bea6d93192159cf04ce541b3
Guilherme Cruz <sambrmg@gmail.com>
2017-09-19 17:21:15 -0300

    Create jsonMenuExample separated

---
+++ js/jsonMenuExample.js
@@ -0,0 +1,50 @@
+var jsonMenu = [
+    { "label": "Amazon", "href": "http://www.amazon.com" },
+    { "label": "Apple", "href": "#", "n": [
+        {
+            "label":"Mac", 
+            "href": "#",
+            "n": [{
+                "label":"Macbook",
+                "href": "https://www.apple.com/macbook/"
+            },{
+                "label":"iMac",
+                "href": "https://www.apple.com/imac/"
+            },{
+                "label":"Mac Pro",
+                "href": "https://www.apple.com/mac-pro/"
+            }]
+        },{
+            "label":"iPhone", 
+            "href": "#",
+            "n": [{
+                "label":"iPhone X",
+                "href": "https://www.apple.com/iphone-x/"
+            },
+            {
+                "label":"iPhone 8",
+                "href": "https://www.apple.com/iphone-8/"
+            }]
+        }
+    ]},
+    { "label": "Asus", "href": "http://www.asus.com" },
+    { "label": "Google", "href": "" , "n": [
+        {
+            "label":"Maps", 
+            "href": "https://www.google.com.br/maps/"
+        },{
+            "label":"My Account", 
+            "href": "#",
+            "n": [{
+                "label":"Security",
+                "href": "https://myaccount.google.com/security"
+            },
+            {
+                "label":"Privacy",
+                "href": "https://myaccount.google.com/privacy"
+            }]
+        }
+    ]},
+    { "label": "Facebook", "href": "http://www.facebook.com" },
+    { "label": "Microsoft", "href": "http://www.microsoft.com" },
+];
